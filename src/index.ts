3133ac6b07c5a7f24a17aad9df16fa88bd4d911b
ShkalikovAV <shkalikov@atb.su>
2024-12-17 19:13:14 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,14 @@ import Sigma from "sigma";
 
 
 const graph = new Graph();
-graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
-graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
-graph.addEdge("1", "2", { size: 5, color: "purple" });
+graph.addNode("1", { label: "Parent 1", x: 0, y: 0, size: 10, color: "red" });
+graph.addNode("2", { label: "Parent 2", x: 10, y: 10, size: 10, color: "red" });
+graph.addNode("1_2", { x: 5, y: 5, size: 5 });
+
+graph.addNode("3", { label: "Node 2", x: 7, y: 3, size: 10, color: "red" });
+
+graph.addEdge("1", "2", { size: 2 });
+graph.addEdge("3", "1_2", { size: 2 });
 
 const renderer = new Sigma(
   graph,
