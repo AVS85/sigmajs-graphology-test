d2e33a9e36214f7a402ea42178dd4920263fa1e8
ShkalikovAV <shkalikov@atb.su>
2024-12-18 04:10:38 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,76 @@
 
 import Graph from "graphology";
 import Sigma from "sigma";
-
+import { circular, rotation } from "graphology-layout"; 
+import family from './mock_family'
+// import { v4 as uuid } from 'uuid';
 
 const graph = new Graph();
-graph.addNode("1", { label: "Parent 1", x: 0, y: 0, size: 10, color: "red" });
-graph.addNode("2", { label: "Parent 2", x: 10, y: 10, size: 10, color: "red" });
-graph.addNode("1_2", { x: 5, y: 5, size: 5 });
 
-graph.addNode("3", { label: "Node 2", x: 7, y: 3, size: 10, color: "red" });
+family.forEach(node => {
+  const { id, label } = node;
+  graph.addNode(id, { label, size: 10})
+})
 
-graph.addEdge("1", "2", { size: 2 });
-graph.addEdge("3", "1_2", { size: 2 });
+// Список id партнёрских нод
+const partnersIdsList = new Set()
+family.forEach(node => !!node?.partnersId ? partnersIdsList.add(node.partnersId) : undefined)
+console.log('partnersIdsList', partnersIdsList);
+
+family.forEach(node => {
+  const {id, partnersId } = node
+  if (partnersId){
+    // Добавляем партнёрские ноды
+    if (!graph.hasNode(partnersId)) {
+      graph.addNode(partnersId, { label: partnersId, size: 10, color: "red" })
+    }
+    // Добавляем связи партнерских нод с родителями
+    if (graph.hasNode(partnersId)) {
+      graph.addEdge(id, partnersId, { size: 2 });
+    }
+    // Добавляем связи партнерских нод с детьми
+    const childNodes = family.filter(node => node.parentsId === partnersId)
+    console.log('childNodes', childNodes);
+    if (childNodes.length) {
+      childNodes.forEach(childNode => {
+        if (!graph.hasEdge(childNode.id, partnersId)) {
+
+          graph.addEdge(childNode.id, partnersId, { size: 2 });
+        }
+      })
+    }
+    
+    // if (graph.hasNode(node.partnersId)) {
+    //   graph.addEdge(node.id, node.partnersId, { size: 2 });
+    // }
+  } 
+})
+// Присоединение родителей к партнерским нодам
+
+// family.forEach(item => {
+
+ 
+// })
+// graph.addNode("1", { label: "Parent 1", size: 10, color: "red" });
+// graph.addNode("2", { label: "Parent 2", size: 10, color: "red" });
+// graph.addNode("1_2", { size: 5 });
+
+// graph.addNode("3", { label: "Child_1_2", size: 10, color: "red" });
+
+// graph.addEdge("1", "1_2", { size: 2 });
+// graph.addEdge("2", "1_2", { size: 2 });
+// graph.addEdge("3", "1_2", { size: 2 });
+
+circular(graph);
+// rotation(graph, 360);
+
+
+// Устанавливаем координаты x и y для каждого узла
+graph.forEachNode((node, attr) => {
+  const coords = graph.getNodeAttributes(node);
+  graph.setNodeAttribute(node, 'x', coords.x || Math.random() * 100); // Устанавливаем случайное значение, если нет
+  graph.setNodeAttribute(node, 'y', coords.y || Math.random() * 100); // Устанавливаем случайное значение, если нет
+});
 
 const renderer = new Sigma(
   graph,
---
+++ src/mock_family.ts
@@ -0,0 +1,51 @@
+export default [
+  {
+    id: '001',
+    label: 'Отец',
+    partnersId: 'ppj3xwaso0k0e07',
+    parentsId: '280ekkfml9e5cor'
+  },
+  {
+    id: '002',
+    label: 'Мать',
+    partnersId: 'ppj3xwaso0k0e07',
+    parentsId: '8epjtypmh43u9nb'
+  },
+  {
+    id: '003',
+    label: 'Сын',
+    partnersId: null,
+    parentsId: 'ppj3xwaso0k0e07'
+  },
+  {
+    id: '004',
+    label: 'Дочь',
+    partnersId: null,
+    parentsId: 'ppj3xwaso0k0e07'
+  },
+  {
+    id: '005',
+    label: 'Дед по отцу',
+    partnersId: '280ekkfml9e5cor',
+    parentsId: null
+  },
+  {
+    id: '006',
+    label: 'Бабушка по отцу',
+    partnersId: '280ekkfml9e5cor',
+    parentsId: null
+  },
+ 
+  {
+    id: '007',
+    label: 'Дед по матери',
+    partnersId: '8epjtypmh43u9nb',
+    parentsId: null
+  },
+  {
+    id: '008',
+    label: 'Бабушка по матери',
+    partnersId: '8epjtypmh43u9nb',
+    parentsId: null
+  }
+]
